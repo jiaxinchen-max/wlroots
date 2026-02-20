9a1b16cc2c912521c6e3623faebc43e197407d97
Chen Jiaxin <chenjiaxin@shopee.com>
2026-02-20 23:05:19 +0800

    add render header file

--- backend/termux/termux_render.c
+++ backend/termux/termux_render.c
@@ -3,6 +3,7 @@
  * serverState, LorieBuffer_lock/unlock, lorie_mutex_lock/unlock, stopEventLoop.
  * render.h brings in tlog from the library's headers; we only use wlr_log.
  */
+#include "termux_render.h"
 #include <termux/render/render.h>
 #include <termux/render/buffer.h>
 #include <stdlib.h>
